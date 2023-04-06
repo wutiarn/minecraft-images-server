32b3c9bd29a7ceb534a9f9483858b14ca46c2cfe
Dmitry Romanov <me@wtrn.ru>
2023-04-06 20:37:07 +0300

    Init

--- mci/telegram.py
+++ mci/telegram.py
@@ -1,6 +1,9 @@
+import requests
+
 from mci import storage, config
 
 _base_url = "https://api.telegram.org"
+_base_methods_url = f"{_base_url}/bot{config.telegram_token}"
 
 
 def handle_event(event: dict):
@@ -13,9 +16,11 @@ def handle_event(event: dict):
         file_id = message["document"]["file_id"]
 
     if file_id:
-        _download_file(file_id)
+        storage.download_file(1, _get_file_download_url(file_id))
 
 
-def _download_file(file_id):
-    url = f"{_base_url}/file/bot{config.telegram_token}/{file_id}"
-    storage.download_file(1, url)
+def _get_file_download_url(file_id: str):
+    response = requests.get(f"{_base_methods_url}/getFile", params={"file_id": file_id})
+    file_path = response.json()["result"]["file_path"]
+    url = f"{_base_url}/file/bot{config.telegram_token}/{file_path}"
+    return url
