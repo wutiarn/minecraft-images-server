a7df7b1971cb066967e3151c131466710d2e2395
Dmitry Romanov <me@wtrn.ru>
2023-04-06 20:51:06 +0300

    WIP: Download files

--- mci/storage.py
+++ mci/storage.py
@@ -1,10 +1,18 @@
 import pathlib
+
+import requests
+
 from mci import config
 
 
 def download_file(id: int, url: str):
     extension = url.split(".")[-1]
     file = _get_storage_dir().joinpath(f"{id}.{extension}")
+    with requests.get(url, stream=True) as response:
+        with open(file) as f:
+            response.raise_for_status()
+            for chunk in response.iter_content(chunk_size=1 * 1024 * 1024):
+                f.write(chunk)
     pass
 
 
