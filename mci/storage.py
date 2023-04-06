a834fbb5fce0d4b245e9c13ad81f5315d6911535
Dmitry Romanov <me@wtrn.ru>
2023-04-06 23:55:43 +0300

    WIP: create table row on file download

--- mci/storage.py
+++ mci/storage.py
@@ -1,24 +1,26 @@
+import datetime
 import pathlib
 
 import requests
 
 from mci import config, db
+import filetype
 
 
 def create_image(url: str):
     with db.get_connection() as c:
         image_id = db.create(c)
-        extension = url.split(".")[-1]
-        file = _get_storage_dir().joinpath(f"{image_id}.{extension}")
+        now = datetime.datetime.now()
+        directory = config.storage_dir.joinpath(str(now.year)).joinpath(f"{now.month:02}")
+        directory.mkdir(parents=True, exist_ok=True)
+        file = directory.joinpath(f"{image_id}.tmp")
         with requests.get(url, stream=True) as response:
             with open(file, "wb") as f:
                 response.raise_for_status()
                 for chunk in response.iter_content(chunk_size=1 * 1024 * 1024):
                     f.write(chunk)
+            type = filetype.guess(file)
+            target_file = directory.joinpath(f"{image_id}.{type.extension}")
+            file = file.rename(target_file)
+        path = str(file.relative_to(config.storage_dir))
     pass
-
-
-def _get_storage_dir() -> pathlib.Path:
-    storage_dir = config.storage_dir
-    storage_dir.mkdir(parents=True, exist_ok=True)
-    return storage_dir
