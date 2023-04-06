df5db9ee4286a06662a91c165bfc3c559f21656c
Dmitry Romanov <me@wtrn.ru>
2023-04-06 20:41:39 +0300

    Download photos

--- mci/telegram.py
+++ mci/telegram.py
@@ -15,12 +15,26 @@ def handle_event(event: dict):
     if "document" in message:
         file_id = message["document"]["file_id"]
 
+    if "photo" in message:
+        file_id = _get_max_photo_resolution_file_id(message["photo"])
+
     if file_id:
         storage.download_file(1, _get_file_download_url(file_id))
 
 
-def _get_file_download_url(file_id: str):
+def _get_file_download_url(file_id: str) -> str:
     response = requests.get(f"{_base_methods_url}/getFile", params={"file_id": file_id})
     file_path = response.json()["result"]["file_path"]
     url = f"{_base_url}/file/bot{config.telegram_token}/{file_path}"
     return url
+
+
+def _get_max_photo_resolution_file_id(photos: list[dict]) -> str:
+    max_file_size = 0
+    file_id = None
+    for photo in photos:
+        file_size = photo["file_size"]
+        if file_size > max_file_size:
+            max_file_size = file_size
+            file_id = photo["file_id"]
+    return file_id
