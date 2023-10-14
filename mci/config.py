760c47a993dc1d0cad75d4586240682257054bfa
Dmitry Romanov <me@wtrn.ru>
2023-10-14 11:34:09 +0300

    Update default TELEGRAM_USER_WHITELIST

--- mci/config.py
+++ mci/config.py
@@ -5,7 +5,7 @@ import pytz
 telegram_token = os.getenv("TELEGRAM_TOKEN")
 storage_dir = pathlib.Path(os.getenv("STORAGE_DIR", "./storage"))
 base_url = os.getenv("BASE_URL", "https://mci.wtrn.ru")
-telegram_user_whitelist = set([int(x) for x in os.getenv("TELEGRAM_USER_WHITELIST", "").split(",")])
+telegram_user_whitelist = set([int(x) for x in os.getenv("TELEGRAM_USER_WHITELIST", "0").split(",")])
 
 timezone = pytz.timezone("Europe/Moscow")
 timestamp_format = "%Y-%m-%d %H:%M:%S %Z"
