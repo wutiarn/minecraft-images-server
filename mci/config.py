f71aa2c805a8a3966d4093ed3bd570ec3c38d414
Dmitry Romanov <me@wtrn.ru>
2023-10-14 15:18:45 +0300

    Remove base_url from config

--- mci/config.py
+++ mci/config.py
@@ -2,6 +2,5 @@ import os
 import pathlib
 
 storage_dir = pathlib.Path(os.getenv("STORAGE_DIR", "./storage"))
-base_url = os.getenv("BASE_URL", "https://mci.wtrn.ru")
 memos_url = os.getenv("MEMOS_URL", "http://192.168.31.3:5230")
 memos_public_url = os.getenv("MEMOS_PUBLIC_URL", memos_url)
